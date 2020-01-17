c4960aa97634eb1c77ab6776f1f3fdbfe2423cb2
kqarryzada <khalid.qarryzada@gmail.com>
2020-01-16 22:40:15 -0600

    Revised 003 to work for single parameter

--- 003/main.c
+++ 003/main.c
@@ -10,9 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// #define DEFAULT_INPUT 600851475143
-#define DEFAULT_INPUT 60
-#define DEBUG 1
+#define DEFAULT_INPUT 600851475143
 
 
 bool is_prime(uint64_t n) {
@@ -39,21 +37,33 @@ bool is_prime(uint64_t n) {
 }
 
 
-// Uses Fermat's factorization algorithm to return the largest prime factor of N. If N is prime, 0
-// is returned.
-uint64_t find_largest_prime_factor(uint64_t _N) {
+// Uses Fermat's factorization algorithm to return the largest prime factor of 'number'. If 'number'
+// is prime, or if a number less than 2 is given, 0 is returned.
+uint64_t find_largest_prime_factor(uint64_t number) {
+    // By definition, 0 and 1 are not prime. 2 is an edge case since it is prime, but even.
+    if (number < 3) {
+        return 0;
+    }
+
     // Per Fermat's factorization algorithm, N should be odd
-    uint64_t N = _N;
+    uint64_t N = number;
     while ((N % 2) == 0) {
         N = N / 2;
     }
+    // After halving N repeatedly, if N == 1, then we know 'number' must be a power of 2.
+    if (N == 1) {
+        return 2;
+    }
 
-    // a = ceil(sqrt(N))
-    uint64_t a = (uint64_t) sqrt((double) N);
-    a++;
-#ifdef DEBUG
-    printf("a starts with value: %ld\n", a);
-#endif
+
+    // Set 'a' as the algorithm requires. a = ceil(sqrt(N))
+    double a_float = sqrt((double) N);
+    uint64_t a = (uint64_t) a_float;
+    // if a == a_float, then a is already equivalent to ceil(sqrt(N)). Otherwise, increment it.
+    if (a != a_float) {
+        a++;
+    }
+
     while (a != N) {
         double b_squared = a * a - N;
         double b_float = sqrt(b_squared);
@@ -62,48 +72,45 @@ uint64_t find_largest_prime_factor(uint64_t _N) {
             // If integer and float values are equal, then b_squared is a perfect square. If that's
             // true, then we have found an "a" and a "b" such that:
             // N^2 = a^2 - b^2 ==> N^2 = (a + b)(a - b)
-            // The algorithm states that a - b will be prime.
-            #ifdef DEBUG
-            printf("a is %ld and b is %ld.\n", a, b);
-            #endif
-            return a - b;
+            uint64_t factor1 = a + b;
+            uint64_t factor2 = a - b;
 
-            // // If the factor we found is not prime, make a recursive call to find the largest prime
-            // // factor of that value.
-            // while (!is_prime(factor1)) {
-            //     prime_factor1 = find_largest_prime_factor(factor1);
-            // }
+            // If either factor is equivalent to N, then N is prime.
+            if (factor1 == N || factor2 == N) {
+                break;
+            }
 
-            // // 
-            // return prime_factor1;
+            // Recursively obtain the largest prime factors of each factor, if they are not already
+            // prime.
+            if (!is_prime(factor1)) {
+                factor1 = find_largest_prime_factor(factor1);
+            }
+            if (!is_prime(factor2)) {
+                factor2 = find_largest_prime_factor(factor2);
+            }
+            
+            return (factor1 > factor2) ? factor1 : factor2;
         }
 
-        // b_squared was not a perfect square this iteration; increment a and try again.
+        // b_squared was not a perfect square this iteration; increment 'a' and try again.
         a++;
     }
 
-    // If we escape the loop, it means N must be prime. If N == _N, then the value given to this
-    // function was prime to begin with, so we return 0 to indicate that _N has no prime factors
-    // other than itself.
-    if (N == _N) {
-        return 0;
-    }
-    else {
-        return N;
-    }
+    // If we escape the loop, it means N must be prime. If N == number, then the value given to this
+    // function was prime to begin with.
+    return (N == number) ? 0 : N;
 }
 
 int main(void) {
     // 'value' is the number whose largest prime factor we are trying to find.
     uint64_t value = DEFAULT_INPUT;
-
-#ifdef DEBUG
-    printf("Value is: %ld.\n", value);
-#endif
+    if (value < 2) {
+        fprintf(stderr, "The value requested was %ld, which is not a valid parameter.\n", value);
+    }
 
     uint64_t factor = find_largest_prime_factor(value);
     if (factor == 0) {
-        printf("There exists no largest prime factor for %ld, since it is a prime number.\n", value);
+        printf("%ld is a prime number, so it does not have a prime factor other than itself.\n", value);
     }
     else {
         printf("The largest prime factor of %ld is %ld.\n", value, factor);
